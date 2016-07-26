5b9598b95ab485a21d6fe33f122f5969f608bd1e
Ruben Weijers <ruben@vita.io>
2016-07-26 11:22:14 +0200

    rename mkDefaultNotifier to defaultNotifier again

--- src/defaultNotifier.js
+++ src/defaultNotifier.js
@@ -1,6 +1,6 @@
 const FUNC_WARNING = `Value is a function. Possibly avoidable re-render?`
 const AVOIDABLE_WARNING = `Value did not change. Avoidable re-render!`
 
-export const mkDefaultNotifier = ({name, prev, next, type}) => {
+export const defaultNotifier = ({name, prev, next, type}) => {
   console.log(`Avoidable re-render in ${name}/${type}:`, prev, next);
 }
