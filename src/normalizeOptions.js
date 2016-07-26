9e10e375f2ff5bf3771a4f61735f2a5d5f4d5828
Ruben Weijers <ruben@vita.io>
2016-07-26 11:16:50 +0200

    add option to limit the amount of traces it will do

--- src/normalizeOptions.js
+++ src/normalizeOptions.js
@@ -4,6 +4,7 @@ import {defaultNotifier} from './defaultNotifier'
 
 export const DEFAULT_INCLUDE = /./
 export const DEFAULT_EXCLUDE = /[^a-zA-Z0-9]/
+export const DEFAULT_LIMIT   = 1000;
 
 const toRegExp = s => _isString(s) ? new RegExp(`^${s}$`) : s
 const toArray = o =>  o ? [].concat(o) : []
@@ -12,12 +13,14 @@ export const normalizeOptions = (opts = {}) => {
   let {
     include = [DEFAULT_INCLUDE],
     exclude = [DEFAULT_EXCLUDE],
-    notifier = defaultNotifier
-  } = opts
+    limit   = DEFAULT_LIMIT,
+    notifier = mkDefaultNotifier(opts.limit || DEFAULT_LIMIT),
+  } = opts;
 
 
   return {
     notifier,
+    limit,
     include: toArray(include).map(toRegExp),
     exclude: toArray(exclude).map(toRegExp)
   }
