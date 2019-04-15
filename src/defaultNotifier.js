6598bb10a176f5785393728704cb6c721baf1d97
Willy Martin <lucky.nowarzz@gmail.com>
2019-04-15 15:59:58 +0700

    Update defaultNotifier.js

--- src/defaultNotifier.js
+++ src/defaultNotifier.js
@@ -3,14 +3,8 @@ const AVOIDABLE_WARNING = `Value did not change. Avoidable re-render!`
 
 export const defaultNotifier = (displayName, diff) => {
   if (diff.length == 0) {
-    if(Reactotron){
-      Reactotron.log( `Avoidable re-render in ${displayName}, no changes`);
-    }
     console.log(`Avoidable re-render in ${displayName}, no changes`);
   } else {
-    if(Reactotron){
-      Reactotron.log(`Avoidable re-render in ${displayName}`, diff);
-    }
     console.log(`Avoidable re-render in ${displayName}`, diff);
   }
 }
